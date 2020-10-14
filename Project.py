a2e5eff542533d2202090d8d8ca9920c4e734b75
Parham <parhamrouzbeh@yahoo.com>
2020-10-14 14:19:57 +0330

    Histogram function has been added

--- Project.py
+++ Project.py
@@ -35,3 +35,15 @@ for person in Persons:
     print("Hello " + person)
 
 #################################
+
+def histogram(items):
+    for n in items:
+        output = ''
+        times = n
+        while times > 0:
+            output += '*'
+            times -= 1
+        print(output)
+histogram([3,6,4,3,6,5])
+
+#################################
