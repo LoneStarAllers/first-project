f8faef157d097243897bf901aa2ad20bff3946e7
Parham <parhamrouzbeh@yahoo.com>
2020-10-14 13:51:23 +0330

    The voewls counter function has been created

--- Project.py
+++ Project.py
@@ -8,3 +8,18 @@ s = 'parham'
 print(reverse(s))
 
 #################################
+
+def vowels_counter(s):
+    sound = "aAeEiIoOuU"
+    count = 0
+    for i in s:
+        if i in sound:
+            count += 1
+    
+    return "There is/are {} vowel chars in \n{}".format(count,s)
+
+s = input("Input a string : ")
+print(vowels_counter(s))
+
+#################################
+
