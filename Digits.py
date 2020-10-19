45d0e245fb5ede695c5dd60fb00a1bc35d36d0a2
Parham <parhamrouzbeh@yahoo.com>
2020-10-19 21:09:56 +0330

    The function has been modified in a way that it processes chunks of 100 numbers of the wholerange and calculates the execution time for each chunk

--- Digits.py
+++ Digits.py
@@ -1,3 +1,4 @@
+import time
 def sumdigits(n):
     if n < 10:
         return n
@@ -12,15 +13,25 @@ def sumdigits(n):
         if digit_count > 1:
             return sumdigits(n) # recursion happens based on the condition
 
-ones_counter = 0
-twos_counter = 0
+start = 0
+stop = 100
+while start < 1000:
+    ones_counter = 0
+    twos_counter = 0
+    start_time = time.time()
 
-for number in range(1000):
-    if sumdigits(number) == 1:
-        print("1 : {}".format(number))
-        ones_counter += 1
-    elif sumdigits(number) == 2:
-        print("2 : {}".format(number))
-        twos_counter += 1
-
-print("there is/are {} 1s and {} 2s".format(ones_counter, twos_counter))
+    print("range {} to {}".format(start,stop-1))
+    for number in range(start, stop):
+        
+        if sumdigits(number) == 1:
+            #print("1 : {}".format(number), end=" ")
+            ones_counter += 1
+        elif sumdigits(number) == 2:
+            #print("2 : {}".format(number), end=" ")
+            twos_counter += 1
+    print()
+    print("--- {} seconds ---".format(time.time() - start_time))
+    print("there is/are {} 1s and {} 2s".format(ones_counter, twos_counter))
+    print()
+    start += 100
+    stop += 100
